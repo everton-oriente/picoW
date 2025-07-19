bd59b2bc2163f9ebabd5a4723c1546b4c922a9bf
everton-oriente <everton.oriente@hotmail.com>
2025-07-19 11:59:51 +0200

    Updated version using watch and display the information trough OLED Display

--- Mon_Temp_Hum_Lum/src/main.rs
+++ Mon_Temp_Hum_Lum/src/main.rs
@@ -5,7 +5,7 @@
  *  Created by  : Everton Oriente
  *  Date        : 2025-06-18
  * -----------------------------------------------------------------------------
- *  Description : 
+ *  Description :
  *      The monitor should acquire information regarding temperature, humidity and luminosity,
  *      and send this information to the SCADA system to hold and store the information regarding the sensors.
  *
@@ -13,49 +13,49 @@
  *  Framework   : Embassy, no_std
  *
  */
-#![no_std]  // (needed for embedded targets)
+#![no_std] // (needed for embedded targets)
 #![no_main] // Disable normal main entry point; we use `#[embassy_executor::main]` instead
 
 // Import required crates and modules
-use cyw43_pio::{PioSpi, DEFAULT_CLOCK_DIVIDER};
+use core::cell::RefCell;
+use core::fmt::Write;
+
+use cyw43_pio::{DEFAULT_CLOCK_DIVIDER, PioSpi};
 use defmt::*; // For logging via RTT
 use embassy_executor::Spawner;
 use embassy_rp::adc::{Adc, Async, Channel, Config as AdcConfig, InterruptHandler as AdcIrq};
 use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{Level, Output, Pull};
-use embassy_rp::peripherals::{DMA_CH0, PIO0, I2C0};
+// Crate regarding I2C Oled Display
+use embassy_rp::i2c::{Async as I2c_async, Config as I2c_config, I2c, InterruptHandler};
+use embassy_rp::peripherals::{DMA_CH0, I2C0, PIO0};
 use embassy_rp::pio::{InterruptHandler as PioIrq, Pio};
-use embassy_time::{Duration, Timer};
-use static_cell::StaticCell;
-use embassy_sync::blocking_mutex::raw::{ThreadModeRawMutex};
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 use embassy_sync::mutex::Mutex;
 //use embassy_sync::channel::{Channel as SyncChannel};
 use embassy_sync::watch::{DynReceiver, Watch};
-use core::cell::RefCell;
-// Crate regarding I2C Oled Display
-use embassy_rp::i2c::{Async as I2c_async, Config as I2c_config, I2c, InterruptHandler};
-use embedded_graphics::{
-    mono_font::{ascii::FONT_6X10, MonoTextStyle},
-    pixelcolor::BinaryColor,
-    prelude::*,
-    text::Text,
-};
+use embassy_time::{Duration, Timer};
+use embedded_graphics::mono_font::MonoTextStyle;
+use embedded_graphics::mono_font::ascii::FONT_6X10;
+use embedded_graphics::pixelcolor::BinaryColor;
+use embedded_graphics::prelude::*;
+use embedded_graphics::text::Text;
 use heapless::String;
-use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
-use core::fmt::Write;
+use micromath::F32Ext;
+use ssd1306::prelude::*;
+use ssd1306::{I2CDisplayInterface, Ssd1306};
+use static_cell::StaticCell;
 // Crate regarding I2C Oled Display
-
 use {defmt_rtt as _, panic_probe as _}; // RTT logging and panic handler
 
-const ADC0_CONSUMERS: usize = 1;
+const ADC0_CONSUMERS: usize = 2;
 static ADC0_CHANNEL: Watch<ThreadModeRawMutex, u16, ADC0_CONSUMERS> = Watch::new();
 
 pub fn get_receiver_adc0() -> Option<DynReceiver<'static, u16>> {
     ADC0_CHANNEL.dyn_receiver()
 }
 
-
-const ADCTEMP_CONSUMERS: usize = 1;
+const ADCTEMP_CONSUMERS: usize = 2;
 static ADCTEMP_CHANNEL: Watch<ThreadModeRawMutex, u16, ADCTEMP_CONSUMERS> = Watch::new();
 
 pub fn get_receiver_adctemp() -> Option<DynReceiver<'static, u16>> {
@@ -71,21 +71,19 @@ bind_interrupts!(struct Irqs {
     ADC_IRQ_FIFO => AdcIrq;
 
     // Bind the interrupt handler to  I2C IRQ
-    I2C0_IRQ => InterruptHandler<I2C0>; 
+    I2C0_IRQ => InterruptHandler<I2C0>;
 
 });
 
 // Bind the interrupt handler to  ADC IRQ
 //bind_interrupts!(struct Irqs {
-    
+
 //});
 
 /// Background task for running the CYW43 Wi-Fi driver.
 /// This must be running at all times for the chip to function.
 #[embassy_executor::task]
-async fn cyw43_task(
-    runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>
-) -> ! {
+async fn cyw43_task(runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>) -> ! {
     runner.run().await
 }
 
@@ -103,10 +101,10 @@ async fn led_blink_task(mut control: cyw43::Control<'static>, delay: Duration) {
     }
 }
 
-// LED blink task - to the gpio in the board 
+// LED blink task - to the gpio in the board
 #[embassy_executor::task]
-async fn toogle_led(mut led: Output<'static>){
-    loop{
+async fn toogle_led(mut led: Output<'static>) {
+    loop {
         info!("LED ON");
         led.set_high();
         Timer::after_millis(2500).await;
@@ -122,7 +120,7 @@ async fn toogle_led(mut led: Output<'static>){
 #[embassy_executor::task]
 async fn read_adc_channels(
     adc_mutex: &'static Mutex<ThreadModeRawMutex, RefCell<Adc<'static, Async>>>,
-    mut chan_0: Channel<'static>,   // Luminosity
+    mut chan_0: Channel<'static>, // Luminosity
     //mut chan_1: Channel<'static>,
     //mut chan_2: Channel<'static>,
     mut chan_temp: Channel<'static>, // Temperature of the DIE
@@ -134,11 +132,11 @@ async fn read_adc_channels(
         let tx_adctemp = ADCTEMP_CHANNEL.sender();
 
         match adc.borrow_mut().read(&mut chan_0).await {
-            Ok(value) =>{ 
+            Ok(value) => {
                 info!("Luminosity: {}", value);
                 // Send the value to the channel
-                
-                tx_adc0.send(value as u16);
+
+                tx_adc0.send(value);
             }
             Err(e) => error!("ADC read error: {}", e),
         }
@@ -163,18 +161,17 @@ async fn read_adc_channels(
                 let voltage = raw as f32 * 3.3 / 4096.0;
                 let temp = 27.0 - (voltage - 0.706) / 0.001721;
                 info!("Temp Die: {} °C (raw: {})", temp, raw);
-                tx_adctemp.send(raw);
+                tx_adctemp.send(raw); // Less the number higher the temperature
             }
             Err(e) => error!("Temp read error: {}", e),
         }
-        Timer::after_millis(4000).await;
+        Timer::after_millis(3800).await;
     }
 }
 
 #[embassy_executor::task]
-async fn process_adc_channel_0(){
-    
-    loop{
+async fn process_adc_channel_0() {
+    loop {
         let mut rx = get_receiver_adc0().unwrap();
         let adc0 = rx.get().await;
         info!("LUZ CHEGOU COM: {}", adc0);
@@ -183,21 +180,20 @@ async fn process_adc_channel_0(){
 }
 
 #[embassy_executor::task]
-async fn process_adc_channel_temp(){
-
-    loop{
+async fn process_adc_channel_temp() {
+    loop {
         let mut rx = get_receiver_adctemp().unwrap();
         let adctemp = rx.get().await;
         info!("TEMP CHEGOU COM: {}", adctemp);
-        Timer::after_secs(5).await;
+        Timer::after_secs(4).await;
     }
 }
 
 #[embassy_executor::task]
 async fn oled_task(i2c: I2c<'static, I2C0, I2c_async>) {
     let interface = I2CDisplayInterface::new(i2c);
-    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
-        .into_buffered_graphics_mode();
+    let mut display =
+        Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0).into_buffered_graphics_mode();
 
     if let Err(_) = display.init() {
         defmt::error!("Display init failed");
@@ -210,30 +206,59 @@ async fn oled_task(i2c: I2c<'static, I2C0, I2c_async>) {
     // Header text
     let header_text = "Smart Vivarium";
     let header_x = (128 - header_text.len() as i32 * 6) / 2;
-    let header_y = 22;
+    let header_y = 12;
 
-    // Dynamic text buffer (replace with actual sensor values later)
-    let mut buffer: String<32> = String::new();
-    core::write!(buffer, "Temp: {}.{} deg C", 25, 712).unwrap();
+    // acquiring the value of the Die Temperature
+    let mut rx_temp = get_receiver_adctemp().unwrap();
+    let mut rx_luminosity = get_receiver_adc0().unwrap();
 
+    loop {
+        let mut buffer_temp: String<32> = String::new(); // Create a buffer to store the text
 
-    let buffer_x = (128 - buffer.len() as i32 * 6) / 2;
-    let buffer_y = 30;
+        let adctemp = rx_temp.get().await; // Get the value of the sensor
+        let adclum = rx_luminosity.get().await; // Get the value of the sensor
 
-    if let Err(_) = display.clear(BinaryColor::Off) {
-        defmt::error!("Clear failed");
-    }
+        let voltage = adctemp as f32 * 3.3 / 4096.0;
+        let temp = 27.0 - (voltage - 0.706) / 0.001721;
+        let temp_trunk = (temp * 100.0).trunc() / 100.0;
+        core::write!(buffer_temp, "Temp: {} C", temp_trunk).unwrap();
 
-    Text::new(header_text, Point::new(header_x, header_y), header_style)
-        .draw(&mut display)
-        .unwrap();
+        let buffer_temp_x = (128 - buffer_temp.len() as i32 * 6) / 2; // Calculate the x-coordinate for the text
+        let buffer_temp_y = 32; // Set the y-coordinate for the text
 
-    Text::new(&buffer, Point::new(buffer_x, buffer_y), temp_style)
-        .draw(&mut display)
-        .unwrap();
+        let mut buffer_lum: String<32> = String::new(); // Create a buffer to store the text
 
-    if let Err(_) = display.flush() {
-        defmt::error!("Flush failed");
+        //let lumens = adclum as f32;  // Convert the value of ADC into Lux
+        //let lumens_trunk = (lumens * 100.0).trunc() / 100.0;
+
+        core::write!(buffer_lum, "Luminosity: {} lux", adclum).unwrap();
+
+        let buffer_lum_x = (128 - buffer_lum.len() as i32 * 6) / 2; // Calculate the x-coordinate for the text
+        let buffer_lum_y = 44;
+
+        // Clear the display
+        if let Err(_) = display.clear(BinaryColor::Off) {
+            defmt::error!("Clear failed");
+        }
+        // Display the text in the OLED Display
+        // Display first line
+        Text::new(header_text, Point::new(header_x, header_y), header_style)
+            .draw(&mut display)
+            .unwrap();
+        // Display second line
+        Text::new(&buffer_temp, Point::new(buffer_temp_x, buffer_temp_y), temp_style)
+            .draw(&mut display)
+            .unwrap();
+        // Display third line
+        Text::new(&buffer_lum, Point::new(buffer_lum_x, buffer_lum_y), temp_style)
+            .draw(&mut display)
+            .unwrap();
+
+        if let Err(_) = display.flush() {
+            defmt::error!("Flush failed");
+        }
+
+        Timer::after_secs(3).await; // Update the display every 3 seconds
     }
 }
 /// Main async entry point
@@ -247,11 +272,11 @@ async fn main(spawner: Spawner) {
     info!("Peripherals has initialized with sucess");
     // Create an Output to the LED
     let blinky_led = Output::new(p.PIN_16, Level::Low); // Led external
-    
+
     // Create an ADC peripheral
     let adc = Adc::new(p.ADC, Irqs, AdcConfig::default());
     // Create a mutex to protect the ADC
-    static ADC: StaticCell<Mutex<ThreadModeRawMutex, RefCell<Adc<'static,Async>>>> = StaticCell::new();
+    static ADC: StaticCell<Mutex<ThreadModeRawMutex, RefCell<Adc<'static, Async>>>> = StaticCell::new();
     // Initialize the ADC
     let adc_mutex = ADC.init(Mutex::new(RefCell::new(adc)));
     // Create the ADC thats read the internal temperature of the DIE, like usage in the watchdog feed, if the temperature goes high we turn off the system
@@ -280,8 +305,8 @@ async fn main(spawner: Spawner) {
     // let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
 
     // Configure power and chip-select GPIOs for the CYW43 chip
-    let pwr = Output::new(p.PIN_23, Level::Low);    // Power control
-    let cs  = Output::new(p.PIN_25, Level::High);   // SPI chip select (active low)
+    let pwr = Output::new(p.PIN_23, Level::Low); // Power control
+    let cs = Output::new(p.PIN_25, Level::High); // SPI chip select (active low)
 
     // Initialize PIO peripheral with interrupts
     let mut pio = Pio::new(p.PIO0, Irqs);
@@ -293,9 +318,9 @@ async fn main(spawner: Spawner) {
         DEFAULT_CLOCK_DIVIDER,
         pio.irq0,
         cs,
-        p.PIN_24,    // SPI SCK
-        p.PIN_29,    // SPI MOSI
-        p.DMA_CH0,   // DMA channel
+        p.PIN_24,  // SPI SCK
+        p.PIN_29,  // SPI MOSI
+        p.DMA_CH0, // DMA channel
     );
 
     // Initialize shared state for the CYW43 driver (required for internal coordination)
@@ -312,7 +337,9 @@ async fn main(spawner: Spawner) {
     control.init(clm).await;
 
     // Enable power-saving mode (optional)
-    control.set_power_management(cyw43::PowerManagementMode::PowerSave).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
 
     // Configure delay for blinking LED
     let delay = Duration::from_millis(5000);
@@ -334,5 +361,4 @@ async fn main(spawner: Spawner) {
 
     // Spawn the I2C Display task
     unwrap!(spawner.spawn(oled_task(i2c)));
-
 }
